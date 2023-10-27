024cb3b5dcccf5cf06e5eb59ad48806fe18f7b12
Mon-her <ingbra12@hotmail.com>
2023-10-27 16:51:15 -0500

    Initial commit

---
+++ force-app/main/default/lwc/prueba27102023/prueba27102023.js
@@ -0,0 +1,19 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { CloseActionScreenEvent } from "lightning/actions";
+export default class Prueba27102023 extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+
+    handleSuccess() {
+    // Close the modal window and display a success toast
+        this.dispatchEvent(new CloseActionScreenEvent());
+        this.dispatchEvent(
+        new ShowToastEvent({
+        title: "Success",
+        message: "Record updated!",
+        variant: "success",
+      }),
+    );
+  }
+}
