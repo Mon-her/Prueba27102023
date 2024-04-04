dd1eb6f342bced358ccfeae6fd3db232d653032c
Mon-her <ingbra12@hotmail.com>
2024-04-04 15:28:38 -0500

    Buckup commit

--- force-app/main/default/lwc/pDFGenerate/pDFGenerate.js
+++ force-app/main/default/lwc/pDFGenerate/pDFGenerate.js
@@ -1,9 +1,12 @@
-import { LightningElement} from 'lwc';
+import { LightningElement } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import JSPDF from '@salesforce/resourceUrl/jsPDFLibrary';
 
 export default class PDFGenerate extends LightningElement {
 	jsPDFInitialized = false;
+	//headers = this.createHeader([
+	//	"Saludo",
+	//]);
 
 	renderedCallback() {
 		if (!this.jsPDFInitialized) {
@@ -21,11 +24,14 @@ export default class PDFGenerate extends LightningElement {
 	handleGeneratePDF() {
 		if (this.jsPDFInitialized) {
 			// Make sure to correctly reference the loaded jsPDF library.
+
 			const doc = new window.jspdf.jsPDF();
+
+
 
 			// Add content to the PDF.
 			doc.text('Hello PDF!', 10, 10);
-
+			//doc.table(30, 30, 'Hello PDF!', this.headers,{autosize:true} );
 			// Save the PDF.
 			doc.save('generated_pdf.pdf');
 		} else {
--- force-app/main/default/lwc/personales/personales.js
+++ force-app/main/default/lwc/personales/personales.js
@@ -1,30 +1,266 @@
-import { LightningElement, api, track, wire} from "lwc";
-//import { ShowToastEvent } from "lightning/platformShowToastEvent";
-//import { CloseActionScreenEvent } from "lightning/actions";
-//import NAME_FIELD from "@salesforce/schema/Personal__c.Name";
+import { LightningElement, api, track, wire } from "lwc";
+import { loadScript } from 'lightning/platformResourceLoader';
+import JSPDF from '@salesforce/resourceUrl/jsPDFLibrary';
+//import autoTableResource from '@salesforce/resourceUrl/jspdf_autotable';
+import jsPDF_AutoTable from '@salesforce/resourceUrl/jsPDF_AutoTable';
 import getbiblio from "@salesforce/apex/pdfprueba.getbiblio";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class Personales extends LightningElement {
+  jsPDFInitialized = false;
+  @api recordId;
+  @api objectApiName;
+  @track outputText;
 
-    @track outputText;
-    @api recordId;
-    @api objectApiName;
-    
-    searchText;
-    @wire(getbiblio, { searchText : "$searchText" })
-    prueba;
-    error;
-    
-    // Evento change
-    handleInputChange(event) {
-      console.log(event);
-      //this.searchText = event.target.value;
-    }
+  @track nombre = '';
+  @track apellido = '';
 
-    // Evento click
-    handleclick(event) {
-      console.log(event);
-      const element = this.template.querySelector('[data-id="overview"]').value;
-      this.searchText = element;
+  searchText;
+  @wire(getbiblio, { searchText: "$recordId" })
+  prueba;
+  error;
+
+  // Evento change
+  handleInputChange(event) {
+    console.log(event);
+    const { name, value } = event.target;
+    this[name] = value;
+  }
+
+  // Evento click
+  handleclick(event) {
+    console.log(event);
+    const element = this.template.querySelector('[data-id="overview"]').value;
+    this.searchText = element;
+    this.outputText = element;
+  }
+
+  renderedCallback() {
+
+    //if (!this.jsPdfInitialized) {
+    // Promise.all([
+    //    loadScript(this, jsPDF),
+    //    loadScript(this, jsPDF_AutoTable)
+    // ])
+    // .then(() => {
+    //     this.jsPdfInitialized = true;
+    // })
+    // .catch(error => {
+    //     console.error('Error al cargar jsPDF y jspdf-autotable', error);
+    // });
+    //}
+
+    if (!this.jsPDFInitialized) {
+      this.jsPDFInitialized = true;
+      loadScript(this, JSPDF)
+      loadScript(this, jsPDF_AutoTable)
+        .then(() => {
+          console.log('jsPDF library loaded successfully');
+        })
+        .catch((error) => {
+          console.error('Error loading jsPDF library', error);
+        });
     }
+  }
+
+  handleGeneratePDF() {
+    //if (this.jsPdfInitialized) {
+
+    const doc = new window.jspdf.jsPDF();
+    //const doc = new window.jspdf.jsPDF();
+    //const inputs = this.template.querySelectorAll('lightning-input');
+    const inputs = this.template.querySelectorAll('[data-id]');
+
+    // Crear un objeto para almacenar los valores de los campos
+    const formData = {};
+    inputs.forEach(input => {
+      formData[input.dataset.id] = input.value;
+    });
+
+    //const x = 10;
+    //const y = 10;
+    //const width = 80;
+    //const height = 10;
+
+    // Dibujar el cuadro
+    //doc.rect(x, y, width, height);
+
+    // Insertar texto dentro del cuadro
+    //const texto = 'Texto dentro del cuadro';
+    //const fontSize = 12;
+    //const textWidth = doc.getTextWidth(texto);
+    //const textX = x + (width - textWidth) / 2; // Centrar horizontalmente el texto dentro del cuadro
+    //const textY = y + (height - fontSize) / 2 + fontSize / 3; // Centrar verticalmente el texto dentro del cuadro
+    // doc.text(texto, textX, textY);
+
+    //doc.text(formData.N, 10, 20);
+
+    // Configurar título del PDF
+    //doc.setFont('helvetica'); // Tipo de fuente
+    //doc.setFontSize(12); // Tamaño de fuente en puntos
+    //doc.setTextColor(255, 0, 0); // Color del texto en RGB (rojo)
+    //doc.setFontSize(22);
+    //doc.text('Datos del Usuario', 10, 10);
+    // Definir los márgenes para mover la tabla en la dirección X
+    const margins = { top: 10, bottom: 10, left: 0 };
+    // Configuración para centrar el texto en las celdas de la tabla
+    const options = {
+      
+      headStyles: { fillColor: [51, 122, 183], textColor: 253 },
+      bodyStyles: { textColor: [255, 0, 255] },
+      //Alinear texto al centro de manera horizontal
+      styles: {halign:'center'}
+    };
+    // Configurar datos de la tabla Nombre
+    const tableTitulo = [
+      ['Formato de credito'],
+    ];
+    doc.autoTable({
+      startY: 10,
+      //startX: 100,
+      //head: tableTitulo,
+      body: tableTitulo,
+      ...options,
+      tableWidth: 200,
+      margin: margins,
+      
+    });
+
+    // Configurar datos de la tabla Nombre
+    const tableData = [
+      ['Nombre de cuenta',
+        'Nombre oportunidad',
+        'Stage oportunidad'
+      ],
+      [formData.overview,
+      formData.N,
+      formData.O
+      ],
+      //['Dirección', direccion]
+    ];
+
+    // Configurar datos de la tabla Edad
+    const tableData_n = [
+      ['Campo', 'Valor'],
+      //['Nombre', formData.N],
+      ['Edad', formData.O],
+      //['Dirección', direccion]
+    ];
+
+    // Agregar tabla al PDF Nombre
+    doc.autoTable({
+      startY: 130,
+      head: tableData.slice(0, 1),
+      body: tableData.slice(1),
+      columnStyles: {
+        0: { cellWidth: 20 }, // Ancho de la primera columna en puntos
+        1: { cellWidth: 'auto' }, // Ancho de la segunda columna ajustado automáticamente al contenido
+        2: { cellWidth: 'wrap' } // Ancho de la tercera columna con contenido que se envuelve
+      }
+      //columnStyles: {
+      // 0: { fillColor: [255, 0, 0] }, // Estilo para la primera columna
+      //  1: { fontStyle: 'italic' } // Estilo para la segunda columna
+      //}
+    });
+
+    // Agregar tabla al PDF Edad
+    doc.autoTable({
+      startY: 80,
+      head: tableData_n.slice(0, 1),
+      body: tableData_n.slice(1),
+      columnWidth: [30, 'auto', 'wrap']
+
+      //columnStyles: {
+      // 0: { fillColor: [255, 0, 0] }, // Estilo para la primera columna
+      //  1: { fontStyle: 'italic' } // Estilo para la segunda columna
+      //}
+    });
+
+    doc.save('formulario.pdf');
+
+    // Mostrar mensaje de éxito
+    ShowToastEvent('Éxito', 'Se ha generado el PDF con éxito', 'success');
+
+    // } else {
+    //  console.error('jsPDF library not initialized');
+    // }
+
+
+
+
+    //if (this.jsPdfInitialized) {
+
+    //const doc = new window.jspdf.jsPDF();
+    //const inputs = this.template.querySelectorAll('lightning-input');
+    //const inputs = this.template.querySelectorAll('[data-id]');
+
+    // Crear un objeto para almacenar los valores de los campos
+    // const formData = {};
+    // inputs.forEach(input => {
+    //   formData[input.dataset.id] = input.value;
+    //});
+
+    //doc.setFontSize(22);
+    // doc.text('Formato de Crédito', 10, 10);
+    //doc.text(formData.N, 10, 20);
+
+    // Obtener los datos del formulario
+    // const nombre = 'Nombre';
+    // const edad = 'Edad';
+    //const direccion = 'Dirección';
+
+    // Configurar opciones de la tabla
+    // const tableData = [
+    //    [nombre, edad, direccion],
+    //     ['John Doe', '30', '123 Street'],
+    //    ['Jane Doe', '25', '456 Avenue']
+    //];
+
+    //doc.autoTable({
+    //     startY: 30,
+    //    head: [['Nombre', 'Edad', 'Dirección']],
+    //    body: tableData,
+    //});
+
+
+  }
+
+  // Llamar a un método para generar el PDF con los datos del formulario
+  // this.generarPDFConDatos(formData);
+
+  // Crear datos para la tabla
+  //const userData = [
+  //  { campo: 'Nombre', valor: 'formData.N' },
+  //  { campo: 'Apellido', valor: 'formData.O' },
+  //];
+
+  // doc.autoTable(['campo', 'valor'], userData.map(row => [row.campo, row.valor]), options);
+
+
+  // Agregar la tabla al PDF
+  //doc.autoTable(['campo', 'valor'], userData.map(row => [row.campo, row.valor]), options);
+
+  //const clienteData = [['Cliente', 'formData.N'], ['Email', 'formData.O']];
+  //doc.autoTable({
+  //  startY: 40,
+  //  head: [['Campo', 'Valor']],
+  // body: clienteData,
+  //});
+
+
+
+  // doc.text(formData, 10, 10);
+  //doc.text('Nombre: ' + formData.N, 10, 20);
+  //doc.text(formData.N, 10, 30);
+  //doc.text(formData.O, 10, 40);
+
+  // this.generarPDFConContenido(formData);
+
+  // Add content to the PDF.
+  //doc.text('Hello PDF!', 10, 10);
+  //doc.text(this.outputText, 10, 10);
+  //doc.text(this.searchText);
+
+  // Save the PDF.
+  //doc.save('generated_pdf.pdf');
 }
